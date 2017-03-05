796cf130a864af719e4dc73bf7a8afdc85f22015
Mohamed Al-khamees <mohamedal0khamees@MOHAMEDs-MacBook-Pro.local>
2017-03-05 10:44:44 -0700

    added album and artist schema

--- database/models/artist.js
+++ database/models/artist.js
@@ -1,1 +1,21 @@
 // Todo: Create Artist Model
+const mongoose=require('mongoose');
+
+const Schema=mongoose.Schema;
+const AlbumSchema=require('./album');
+
+
+const ArtistSchema=new Schema({
+  name: String,
+  age: Number,
+  yearActive: String,
+  image: String,
+  genre: String,
+  website: String,
+  netWorth:Number,
+  labelName: String,
+  retired: Boolean,
+  album: [AlbumSchema]
+})
+const Artist=mongoose.model(ArtistSchema, 'artist');
+module.exports=Artist;
